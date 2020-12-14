2209d44b1c267bce8f821665432b32d60c0faaca
navodpro <66838043+navodpro@users.noreply.github.com>
2020-12-14 07:01:42 -0800

    Update _help.py

--- telebot/plugins/_help.py
+++ telebot/plugins/_help.py
@@ -19,7 +19,7 @@ import os
 from telebot import ALIVE_NAME, CMD_HELP, CMD_HNDLR, CMD_LIST
 from telebot.telebotConfig import Config
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TeleBot User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "NavodUserBot User"
 CMD_HNDLR = Config.CMD_HNDLR
 CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "⚡")
 
@@ -60,18 +60,18 @@ async def cmd_list(event):
                 if input_str in CMD_HELP:
                     for i in CMD_HELP[input_str]:
                         string += i
-                    string += "\n\n**© @TeleBotSupport**"
+                    string += "\n\n**© @Mr_dutugemunu**"
                     await event.edit(string)
                 else:
                     for i in CMD_LIST[input_str]:
                         string += "    " + i
                         string += "\n"
-                    string += "\n**© @TeleBotSupport**"
+                    string += "\n**© @Mr_dutugemunu**"
                     await event.edit(string)
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[TeleBot](https://xditya.gitbook.io/telebot/)**\n\n"""
+            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[NavodUserBot](https://github.com/navodpro/TeleBot/)**\n\n"""
             try:
                 results = await bot.inline_query(  # pylint:disable=E0602
                     tgbotusername, help_string
